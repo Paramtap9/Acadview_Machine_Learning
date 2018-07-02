c79cdeaf58ded33fca637251d2e5669f9f2d9235
Ashutosh <assassashu007@gmail.com>
2018-07-02 17:00:44 +0530

    2

---
+++ assignment_2.py
@@ -0,0 +1,8 @@
+print("Q.7- Create a user defined dictionary and get keys corresponding to the value using for loop.")
+
+keys = for x in range(A,H)
+values = for y in range(1,8)
+print(keys)
+print(values)
+dict = {}
+pirnt("Enter no of values in dictionary dictionary")
