c68ccbd09681255c4ed0fa52767e767ec5455a1a
Ashutosh <assassashu007@gmail.com>
2018-07-02 16:57:25 +0530

    1

---
+++ assignment_1.py
@@ -0,0 +1,24 @@
+print("\n\nQ.3- Create a list of integer elements by user input. Make a new list which will store square of elements of previous list.")
+
+n=int(input("Enter the number of elements : "))
+a=[0]*n
+b=[0]*n
+
+i=0
+while i < n:
+    a[i] = int(input("Enter elements : ")
+    i +=1
+
+
+i=0
+while i < n:
+    print
+    b[i] = b[i] * b[i]
+    i +=1
+
+
+print("Squares are : ")
+i=0
+while i < n:
+    print(b[i])
+    i +=1
