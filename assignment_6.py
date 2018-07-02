fb60befa0efd943fb8342e47e730f01504365ed7
Ashutosh <assassashu007@gmail.com>
2018-07-02 17:03:11 +0530

    6

---
+++ assignment_6.py
@@ -0,0 +1,9 @@
+print("\n\nQ.3- Print multiplication table of 12 using recursion.\n")
+
+def table():
+    a = 1
+    while a<=10:
+        print("12  *  " + str(a) + "  =  " +str(12*a))
+        a+=1
+
+table()
