ef9b373ee8628ee1e8b01dd3e7bf82d690efe2e5
Paramtap9 <33710650+Paramtap9@users.noreply.github.com>
2018-07-07 17:18:03 +0530

    Update assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -1,24 +1,33 @@
-print("\n\nQ.3- Create a list of integer elements by user input. Make a new list which will store square of elements of previous list.")
+print("Q.1- Print anything you want on screen. ")
 
-n=int(input("Enter the number of elements : "))
-a=[0]*n
-b=[0]*n
+print("ASSINGMENT 1\n\n\n")
 
-i=0
-while i < n:
-    a[i] = int(input("Enter elements : ")
-    i +=1
+print("Q.2- Join two strings using '+' E.g.-'Acad'+'View'.")
 
+print("Acad"+"View\n\n\n")
 
-i=0
-while i < n:
-    print
-    b[i] = b[i] * b[i]
-    i +=1
+print("Q.3- Take the input of 3 variables x, y and z . Print their values on screen.") 
 
+x = input("Enter x : ")
+y = input("Enter y : ")
+z = input("Enter z : ")
+print("Values of x , y and z are "+ x +" , "+ y +" and "+ z +" respectively.\n\n\n")
 
-print("Squares are : ")
-i=0
-while i < n:
-    print(b[i])
-    i +=1
+print("Q.4- Print “Let’s get started” on screen.") 
+
+print("Let's get started!\n\n\n")
+
+print("Q.5- Print the following values using placeholders. s=”Acadview” course=”Python” fees=5000.")
+
+s="Acadview"
+course="Python"
+fees = 5000
+print("s = " + s)
+print("course = " + course)
+print("fees = " + str(fees) + "\n\n\n")
+
+print("Q.6- Let’s do some interesting exercise: name=”Tony Stark”   salary=1000000   print(‘%s’’%d’)%(____ ,____).")
+
+name="Tony Stark"
+salary=1000000
+print(name + "  " + str(salary))
